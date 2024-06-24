025f98c8f84b593283c6bb5ef58f148d917d5240
PeterJLewis <102032656+PeterJLewis@users.noreply.github.com>
2024-06-24 15:17:48 +0100

    Fixed Pip2 install issues

--- Setup.sh
+++ Setup.sh
@@ -16,14 +16,15 @@ source V2GInjector/bin/activate
 pip install -r requirments.txt
 cd V2GInjector
 git submodule update --init --recursive
-# Scapy Downgrade
+# Python3 Scapy Downgrade
 pip uninstall scapy
 pip install "scapy==2.4.4"
 # pip2 Installation
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
-python2 get-pip.py
+sudo python2 get-pip.py
 # Scapy for python2
-pip2 install "scapy==2.4.4"
+sudo pip2 install setuptools
+sudo pip2 install "scapy==2.4.4"
 # V2GDecoder Setup
 wget https://github.com/FlUxIuS/V2Gdecoder/releases/download/v1/V2Gdecoder.jar --directory-prefix=bin
 rm V2GDecoder_default_run.sh
